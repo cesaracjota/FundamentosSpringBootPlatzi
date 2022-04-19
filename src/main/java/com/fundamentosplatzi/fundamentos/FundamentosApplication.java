a73765c1351d9e535e8aef6a03c79f5049fb5f44
Cesar Acjota Merma <cesar.acjota@tecsup.edu.pe>
2022-04-19 17:33:37 -0500

    first commit

---
+++ src/main/java/com/fundamentosplatzi/fundamentos/FundamentosApplication.java
@@ -0,0 +1,13 @@
+package com.fundamentosplatzi.fundamentos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundamentosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FundamentosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fundamentosplatzi/fundamentos/FundamentosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fundamentosplatzi.fundamentos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FundamentosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
